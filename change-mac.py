7fafc4556e86ccfcc79bb2fa5b6d795bd4c71caa
pauloferrti <pauloferrti@gmail.com>
2022-09-28 18:03:45 -0300

    c3 - change functions to prevent commands injection

--- change-mac.py
+++ change-mac.py
@@ -5,6 +5,7 @@ new_mac = input("new MAC > ") # get the MAC address
 
 print(f"\n[+] Changing MAC address for {interface} to {new_mac}")
 
-subprocess.call(f"ifconfig {interface} down", shell=True)
-subprocess.call(f"ifconfig {interface} hw ether {new_mac}", shell=True)
-subprocess.call(f"ifconfig {interface} up", shell=True)
+
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+subprocess.call(["ifconfig", interface, "up"])
