22e8c813a829e889cafe951af682aa1f8787fdce
pauloferrti <pauloferrti@gmail.com>
2022-09-28 17:38:06 -0300

    c2 - add functions to access shell

--- change-mac.py
+++ change-mac.py
@@ -2,3 +2,9 @@ import subprocess
 
 interface = input("interface > ") # get the interface to change
 new_mac = input("new MAC > ") # get the MAC address
+
+print(f"\n[+] Changing MAC address for {interface} to {new_mac}")
+
+subprocess.call(f"ifconfig {interface} down", shell=True)
+subprocess.call(f"ifconfig {interface} hw ether {new_mac}", shell=True)
+subprocess.call(f"ifconfig {interface} up", shell=True)
