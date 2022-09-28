31073ea8a9e6cbc206c869bc0fc95f128d4f4ef1
pauloferrti <pauloferrti@gmail.com>
2022-09-28 18:26:06 -0300

    c4 - add helper

--- change-mac.py
+++ change-mac.py
@@ -1,4 +1,12 @@
 import subprocess
+import optparse 
+
+parser = optparse.OptionParser()
+
+parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+
+parser.parse_args()
 
 interface = input("interface > ") # get the interface to change
 new_mac = input("new MAC > ") # get the MAC address
