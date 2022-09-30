6a9141b5650092bcc7a04496fb80824c580a8753
pauloferrti <pauloferrti@gmail.com>
2022-09-30 10:24:44 -0300

    c7 - create change mac function

--- change-mac.py
+++ change-mac.py
@@ -1,6 +1,13 @@
 import subprocess
 import optparse 
 
+# function need an interface and a mac address as arguments
+def change_mac(interface, new_mac):
+    print(f"\n[+] Changing MAC address for {interface} to {new_mac}")
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
 parser = optparse.OptionParser()
 
 parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
@@ -8,12 +15,4 @@ parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
 
 (options, arguments) = parser.parse_args()
 
-interface = options.interface  # get the interface to change
-new_mac = options.new_mac # get the MAC address
-
-print(f"\n[+] Changing MAC address for {interface} to {new_mac}")
-
-
-subprocess.call(["ifconfig", interface, "down"])
-subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-subprocess.call(["ifconfig", interface, "up"])
+change_mac(options.interface, options.new_mac)
