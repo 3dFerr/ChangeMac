f3ad2b3f07cd4bb2f319d7123994679a5888b6c6
pauloferrti <pauloferrti@gmail.com>
2022-09-30 10:52:23 -0300

    c9 - validate user input

--- change-mac.py
+++ change-mac.py
@@ -7,7 +7,12 @@ def get_arguments():
 
     parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
     parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
-    return parser.parse_args()
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Please specify an interface, use --help for more info.")
+    elif not options.new_mac:
+        parser.error("[-] Please specify a new mac, use --help for more info.")
+    return options 
 
 # function need an interface and a mac address as arguments
 def change_mac(interface, new_mac):
@@ -18,6 +23,6 @@ def change_mac(interface, new_mac):
 
 
 
-(options, arguments) = get_arguments()
+options = get_arguments()
 
 change_mac(options.interface, options.new_mac)
