8e830ac5bbf9516e0b638ceb42cb897693dddf97
pauloferrti <pauloferrti@gmail.com>
2022-09-30 10:56:42 -0300

    c10 - minor fix

--- change-mac.py
+++ change-mac.py
@@ -8,6 +8,7 @@ def get_arguments():
     parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
     parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
     (options, arguments) = parser.parse_args()
+    
     if not options.interface:
         parser.error("[-] Please specify an interface, use --help for more info.")
     elif not options.new_mac:
